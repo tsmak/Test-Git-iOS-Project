5af0e5164faa48611ce89ea854cf437e9d6fa180
ts <ts@tss-Mac.local>
2014-05-02 02:33:00 -0700

    add CCTest.h and CCTest.m

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by ts on 5/2/14.
+//  Copyright (c) 2014 mak Learning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by ts on 5/2/14.
+//  Copyright (c) 2014 mak Learning. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
